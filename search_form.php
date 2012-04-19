e4731906f8ffb4002131fc1a056a7a6763153c93
Alex Ciarlillo <alex.ciarlillo@gmail.com>
2012-04-19 10:24:19 -0400

    minor changes to text and links

--- search_form.php
+++ search_form.php
@@ -18,17 +18,18 @@
                 <input id="ebscohostsearchmode" name="ebscohostsearchmode" type="hidden" value="+AND+" />
                 <input id="ebscohostkeywords" name="ebscohostkeywords" type="hidden" value="" />
                 <div>
-                    <h2 class="searchtext">Search articles and more</h2><br/>
+                    <h2 class="searchtext">Find articles and more</h2><br/>
                     <input class="searchinput" id="ebscohostsearchtext" name="ebscohostsearchtext" type="text" />
                     <input class="searchsubmit" type="submit" value="Search" />
                 </div>
+                <a class="searchlink" href="http://search.ebscohost.com/login.aspx?direct=true&site=eds-live&scope=site&type=1&custid=s8447892&groupid=main&profid=eds&mode=and&cli0=FT1&clv0=Y&authtype=ip,guest" target="_blank">Advanced Search</a>
             </form>
             <!-- EBSCOhost Custom Search Box Ends -->
         </div>
 
         <div class="searchtab" id="alice" class="ui-tabs-hide ui-tabs-panel">
             <form id="alicesearch" name="alicesearch" action="" onsubmit="window.open(AliceURL()); return false;"  method="post">
-                <h2 class="searchtext">Search for books, journal titles, videos, music, and more</h2><br/>
+                <h2 class="searchtext">Find books, journal titles, videos, music, and more</h2><br/>
                 <select id="aliceselect" style="width:120px;">
                     <option value="k">Keyword</option>
                     <option value="t">Title</option>
@@ -44,16 +45,16 @@
     
         <div class="searchtab" id="erm" class="ui-tabs-hide ui-tabs-panel">
             <form id="ermsearch" name="ermsearch" action="" onsubmit="window.open(ErmURL()); return false;"  method="post">
-                <h2 class="searchtext">Find a database</h2><br/>
+                <h2 class="searchtext">Search for Databases by Name</h2><br/>
                 <input class="searchinput" type="text" value="" name="SEARCH">
                 <input class="searchsubmit" type="submit" value="Search">
-                <a class="searchlink" href="http://alice.library.ohiou.edu/search/e#" target="_blank">Advanced Search</a>
+                <a class="searchlink" href="http://alice.library.ohiou.edu/search/e#" target="_blank">Browse by Subject</a>
             </form>
         </div>
 
         <div class="searchtab" id="cr" class="ui-tabs-hide ui-tabs-panel">
             <form id="crsearch" name="crsearch" action="" onsubmit="window.open(ReservesURL()); return false;" method="post">
-                <h2 class="searchtext">Search for materials by course or instructor</h2><br/>
+                <h2 class="searchtext">Find materials by course or instructor</h2><br/>
                 <select id="crselect" style="width: 120px;">
                     <option value="r">Course</option>
                     <option value="p">Instructor</option>
@@ -65,7 +66,7 @@
 
         <div class="searchtab" id="scg" class="ui-tabs-hide ui-tabs-panel">
             <form id="scgsearch" name="scgsearch" action="" onsubmit="window.open(LibGuidesURL()); return false;" method="post">
-                <h2 class="searchtext">Search subject and course-specific research guides</h2><br/>
+                <h2 class="searchtext">Find subject and course-specific research guides</h2><br/>
                 <input class="searchinput" type="text" value="" name="SEARCH">
                 <input class="searchsubmit" type="submit" value="Search">
             </form>
